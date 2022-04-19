7dfef94af02ec7a0ae453f4e327a87e9c39d23d4
Satyam Singh <satyam010singh@gmail.com>
2022-04-20 00:27:54 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,30 +1,29 @@
-import cv2         # Library for openCV
+import cv2         
 import time
-import threading   # Library for threading -- which allows code to run in backend
-import playsound   # Library for alarm sound
+import threading   
+import playsound   
 
-har_classifier = cv2.CascadeClassifier('fire_detection.xml') # To access xml file which includes positive and negative images of fire. (Trained images)
-                                                                         # File is also proweb_cameraed with the code.
+har_classifier = cv2.CascadeClassifier('fire_detection.xml') 
 
-web_camera = cv2.VideoCapture(0) # To start camera this command is used "0" for laptop inbuilt camera and "1" for USB attahed camera
+web_camera = cv2.VideoCapture(0) 
 
-def initiate_alarm(): # defined function to play alarm post fire detection using threading
-    playsound.playsound('fire_alarm2.mp3',True) # to play alarm # mp3 audio file is also proweb_cameraed with the code.
-    # print("alarm shut down !") # to print in console
+def initiate_alarm(): 
+    playsound.playsound('fire_alarm2.mp3',True) 
+    # print("alarm shut down !") 
 
 		
 while(True):
-    ret, frame = web_camera.read() # Value in ret is True # To read web_cameraeo frame
+    ret, frame = web_camera.read() 
     if ret == True:        
-        grayscale_camera_frames = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # To convert frame into gray color
-        fire = har_classifier.detectMultiScale(grayscale_camera_frames, 1.2, 5) # to proweb_camerae frame resolution
+        grayscale_camera_frames = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+        fire = har_classifier.detectMultiScale(grayscale_camera_frames, 1.2, 5) 
 
-        ## to highlight fire with square 
+        
         for (x,y,w,h) in fire:
             #print("Fire Detected !")
             cv2.rectangle(frame,(x-10,y-10),(x+w+10,y+h+10),(255,0,0), 3)
             print("Fire alarm started !")
-            threading.Thread(target=initiate_alarm).start()  # To call alarm thread
+            threading.Thread(target=initiate_alarm).start()
 
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
